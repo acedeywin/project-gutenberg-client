61fb25de91b35083e569ab1182fa20c89b9f8e82
Stephen Okpalaononuju <stephenokpala@gmail.com>
2024-10-16 06:38:32 +0100

    ch: fix type error

--- src/app/redux/Provider.tsx
+++ src/app/redux/Provider.tsx
@@ -4,7 +4,8 @@ import { Provider } from 'react-redux';
 import { store, persistor } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
 
-function Providers({ children }) {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+function Providers({ children }: any) {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
